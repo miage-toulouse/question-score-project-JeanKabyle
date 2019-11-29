fb084378c1181cca999036e9c0418ce1dbbe9891
bhs2235a <samir.bouharira@yahoo.fr>
2019-11-29 14:29:31 +0100

    fix #2 Tests sur QuestionAChoixMultiple

---
+++ src/test/java/miagem1/ScoreCalculateurTest.java
@@ -0,0 +1,48 @@
+package miagem1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ScoreCalculateurTest {
+
+    private QuestionAChoixMultiple questionAChoixMultiple;
+    private ScoreCalculateur scoreCalc;
+
+
+    //given
+    @Before
+    public void setUp() throws Exception{
+        questionAChoixMultiple = new QuestionAChoixMultiple("q1", new ArrayList<Integer>(Arrays.asList(2, 3, 5)));
+        scoreCalc = new ScoreCalculateur();
+    }
+
+    @Test
+    public void calculeScoreMauvaisesReponses() {
+        //When
+        float resScore = scoreCalc.calculeScore(new ArrayList<Integer>(Arrays.asList(1, 4)), questionAChoixMultiple);
+        //Then
+        assertEquals(0f,resScore,0.01f);
+    }
+
+    @Test
+    public void calculeScoreBonnesReponsesPartielles() {
+        //When
+        float resScore = scoreCalc.calculeScore(new ArrayList<Integer>(Arrays.asList(2, 3)), questionAChoixMultiple);
+        //Then
+        assertEquals(2f*100f/3f,resScore,0.01f);
+    }
+
+    @Test
+    public void calculeScoreBonnesReponses() {
+        //When
+        float resScore = scoreCalc.calculeScore(new ArrayList<Integer>(Arrays.asList(2, 3, 5)), questionAChoixMultiple);
+        //Then
+        assertEquals(100f,resScore,0.01f);
+    }
+}
